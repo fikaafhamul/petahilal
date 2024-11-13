25a4d29861cd1a97d400c20548b8c465b7464305
fikaafhamul <166165920+fikaafhamul@users.noreply.github.com>
2024-11-13 16:50:45 +0700

    Update hilal.py

--- hilal.py
+++ hilal.py
@@ -24,9 +24,9 @@ class awalbulan:
         self.TH = TH
         self.kriteria = kriteria
         self.JDE = self.hitung_jde()  # Menghitung JDE saat inisialisasi
-        self.konjungsi = self.new_moon()  # Mengambil nilai konjungsi saat inisialisasi
+        self.newmoon = self.new_moon()  # Mengambil nilai konjungsi saat inisialisasi
         self.moonrise_moonset = self.rise_set_moon()
-        self.sunset, self.moonset, self.moonage, self.juliandatum = self.calculate_hilal()  # Simpan hasil ke atribut
+        self.konjungsi, self.jd, self.sunset, self.moonset, self.moonage, self.juliandatum = self.calculate_hilal()  # Simpan hasil ke atribut
 
     def hitung_jde(self):
         # Menghitung Hy
@@ -76,7 +76,7 @@ class awalbulan:
         
     def calculate_hilal(self):
         # Ambil waktu konjungsi pertama
-        konjungsi_time = self.konjungsi[0]
+        konjungsi_time = self.newmoon[0]
 
         # Menentukan rentang waktu untuk pencarian matahari terbenam
         t0 = ts.utc(konjungsi_time.year, konjungsi_time.month, konjungsi_time.day, 0, 0)
@@ -139,7 +139,55 @@ class awalbulan:
         elif kriteria == "NEO MABIMS":
             jd = (t0 + timedelta(days=1)) if (alt >= 3 and el_geo >= 6.4) else (t0 + timedelta(days=2))
 
-        jd = jd + timedelta(days=28)
-        print(jd.astimezone(ZonaWaktu))
+        jd = jd + timedelta(days=28) + self.TH
+
+        # Mengatur rentang waktu untuk pencarian New Moon
+        t0 = jd - timedelta(days=2)
+        t1 = jd + timedelta(days=2)
+
+        # Mencari fase bulan
+        phases_time, y = almanac.find_discrete(t0, t1, almanac.moon_phases(e))
+
+        # Mengambil waktu New Moon
+        new_moon_times = phases_time[y == 0]  # 0 menandakan New Moon
+        new_moon_times = new_moon_times.astimezone(ZonaWaktu)
+
+        konjungsi_times = new_moon_times[0]
+
+        sunriset, sunBol = almanac.find_discrete(t0, t1, almanac.sunrise_sunset(e, longlat))
+        sunset_time = sunriset[sunBol == 0]  # 0 menandakan waktu terbenam
+
+        # Ubah ke waktu UTC
+        sunset_time_utc = sunset_time.utc_iso()
+
+        # Ubah ke waktu lokal
+        sunset_time_local = sunset_time.astimezone(ZonaWaktu)
+
+        # Menghitung Umur Bulan
+        temp = konjungsi_time.hour + (konjungsi_time.minute)/60 + (konjungsi_time.second)/3600
+        temp1 = sunset_time_local[0]
+        temp1 = temp1.hour + (temp1.minute)/60 + (temp1.second)/3600
+        moonage = (temp1 - temp)
+
+        # Menghitung waktu terbenam Bulan
+        moonriset, moonBol = almanac.find_discrete(t0, t1, self.moonrise_moonset)
+        moonset_time = moonriset[moonBol == 0]
+
+        # Ubah ke waktu UTC
+        moonset_time_utc = moonset_time.utc_iso()
+
+        # Ubah ke waktu lokal
+        moonset_time_local = moonset_time.astimezone(ZonaWaktu)
+
+        # Menghitung Tinggi dan Elongasi Bulan
+        geo_moon = earth.at(sunset_time[0]).observe(moon).apparent()
+        geo_sun = earth.at(sunset_time[0]).observe(sun).apparent()
+        el_geo = geo_sun.separation_from(geo_moon).degrees
+
+        topo_moon = observer.at(sunset_time[0]).observe(moon).apparent()
+        topo_sun = observer.at(sunset_time[0]).observe(sun).apparent()
+        alt, az, distance = topo_moon.altaz()
+        alt = alt.degrees
+        el_topo = topo_sun.separation_from(topo_moon).degrees
         
-        return sunset_time_local, moonset_time_local, moonage, jd
+        return konjungsi_times, jd, sunset_time_local, moonset_time_local, moonage
