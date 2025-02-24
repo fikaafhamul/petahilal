e9bd5d520153be233286ec416f7f7636a5943600
fikaafhamul <166165920+fikaafhamul@users.noreply.github.com>
2025-02-24 23:51:02 +0700

    Update hilal.py

--- hilal.py
+++ hilal.py
@@ -35,7 +35,7 @@ def visibilitas_oddeh(sunset, lag, separasi, lebar_sabit):
     return best_time, q, parameter
 
 class awalbulan:
-    def __init__(self, bulan, tahun, lok, lat, lon, TZ='Asia/Jakarta', TT = 0, TH = 0, kriteria = 'NEO MABIMS', id_cuaca = ' ', jam_cuaca = ' '):
+    def __init__(self, bulan, tahun, lok, lat, lon, TZ='Asia/Jakarta', TT = 0, TH = 0, kriteria = 'NEO MABIMS', id_cuaca = '', jam_cuaca = ''):
         self.bulan = bulan
         self.tahun = tahun
         if self.bulan < 2:
@@ -51,7 +51,7 @@ class awalbulan:
         self.TT = TT
         self.TH = TH
         self.kriteria = kriteria
-        if id_cuaca !== " ":
+        if id_cuaca != '':
             self.id_cuaca = id_cuaca
             self.jam_cuaca = jam_cuaca
         self.JDE = self.hitung_jde()  # Menghitung JDE saat inisialisasi
